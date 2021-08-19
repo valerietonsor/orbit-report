3a7d9b75ae962dd38cd900042f7e738dae9433dd
vtonsor <30914395+vtonsor@users.noreply.github.com>
2021-08-18 20:35:21 -0500

    "first commit"

---
+++ The Complete Web Developer Course 2.0/1-html/index.php
@@ -0,0 +1,122 @@
+<DOCTYPE! html> 
+<html>
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+ <head>
+        <title>Yoga With Adriene</title>
+</head>
+    
+    <body>
+        <p>Mini recreate of yogawithadriene.com homepage for education assignment.</p>
+        <h1 id="top">Yoga With Adriene</h1>
+        
+        <img src="adriene.png" width="200px" alt="Adriene">
+        
+        <h2>Yoga is really the art of waking up.</h2>
+        <p>Getting back to the true you. It can be that simple. Yoga offers up a way for us to see a world that is working for you instead of against you. Yoga reminds me that everything is connected so we must live, act, breathe with awareness. The process is the candy. Enjoy!</p>
+        
+        <h2>Subscibe to my Weekly Letter</h2>
+        <form>
+            <input type="text" placeholder="First Name">
+            <br>
+            <input type="text" placeholder="Email">
+            <br>
+            <input type="submit" value="SUBSCRIBE">
+        </form>
+        
+        <h2>This Month ... Our Community Theme is INQUIRE.</h2>
+        <p>On a mission to get the tools of yoga into schools and homes, Adriene hosts the YouTube channel, <a href="https://www.youtube.com/user/yogawithadriene/featured">Yoga with Adriene</a>, a global online community of over 7 million subscribers.</p>
+        
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/hoGJXCg5yb0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        
+        <button>
+            <a href="https://www.youtube.com/user/yogawithadriene/videos"></a>Watch Yoga With Adriene on YouTube
+        </button>
+        
+        <hr> 
+        
+      <a href="https://yogawithadriene.com/adriene-mishler/"> <img src="about-adriene.jpg" height="150px" align="left" alt="Adriene"></a>  
+        <h2>About Adriene</h2>
+        <p>Adriene Mishler is an actress, writer, international yoga teacher and entrepreneur from Austin, Texas. On a mission to get the tools of yoga into schools and homes, Adriene hosts the YouTube channel <a href="https://www.youtube.com/user/yogawithadriene/videos">Yoga with Adriene</a>, an online community of over 7 million subscribers.
+            <br>
+        Yoga with Adriene provides high quality practices on yoga and mindfulness at no cost to inspire people of all ages, shapes and sizes across the globe.</p>
+        <hr>
+        
+       <a href="videos.html"><h3>Many classes to choose from...</h3></a> 
+        <ul>
+            <li>Awaken the <sup>Artist</sup> Within</li>
+            <li>Fundaments of Ease</li>
+            <li>Yoga for Focus & Productivity</li>
+            <li> ... and <u>many</u> more</li>
+        </ul>
+        
+        <table>
+            <thead>
+                <tr>
+                    <th>Practice</th>
+                    <th>Length</th>
+                    <th>Notes</th>
+                </tr>
+            </thead>
+        
+            <tbody>
+                <tr>
+                    <td>Awake the Artist Within</td>
+                    <td>40 Minutes</td>
+                    <td>Total refresh</td>
+                </tr>
+            
+                <tr>
+                    <td>Fundamentals of Ease</td>
+                    <td>35 minutes</td>
+                    <td>Beginner</td>
+                 </tr>
+                
+                <tr>
+                    <td>Yoga for Focus and Productivity</td>
+                    <td>10 minutes</td>
+                    <td>Reset</td>
+                </tr>
+            </tbody>
+        </table>
+        
+        <hr>
+        
+        <h3>YWA Survey</h3>
+        <h4>Which classes have you taken? <em>(Check all that apply.)</em></h4>
+        <form>
+            <input type="checkbox" value="awaken">Awaken the Artist Within
+            <br> 
+            <input type="checkbox" value="ease">Fundamentals of Ease     
+            <br>
+            <input type="checkbox" value="focus">Yoga for Focus & Productivity   
+            
+            <h4>Did you enjoy your yoga experience?</h4>
+            <input type="radio" value="yes" name="yn">Yes
+            <input type="radio" value="no" name="yn">No
+            
+            <h4>One word to describe my yoga experience is...</h4>
+            <input type="text">
+            
+            <h4>Favorite Yoga Teacher</h4>
+            <select>
+                <option>Adriene</option>
+                <option>Adriene</option>
+                <option>Adriene</option>
+                <option>Adriene</option>
+                <option>Adriene</option>
+                <option>Adriene</option>
+            </select>
+            <br>
+            <br>
+            <br>
+            <input type="submit" value="Send">
+        
+            
+        
+        </form>
+        
+        <a href="#top">Return to the top.</a>
+        </body>
+    </html>
