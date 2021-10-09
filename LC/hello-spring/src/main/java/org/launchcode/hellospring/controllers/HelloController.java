0a9d69ddbf0c389419eff305118d6f012c7f4945
vtonsor <30914395+vtonsor@users.noreply.github.com>
2021-10-08 19:06:08 -0500

    initial commit

--- LC/hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ LC/hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,2 +1,63 @@
-package org.launchcode.hellospring.controllers;public class HelloController {
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Chris Bay
+ */
+@Controller
+public class HelloController {
+
+    //    // Handle request at path /hello
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
+
+    @GetMapping("goodbye")
+    @ResponseBody
+    public String goodbye() {
+        return "Goodbye, Spring!";
+    }
+
+    // Handles requests of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
+    }
+
+    // Handles requests of the form /hello/LaunchCode
+    @GetMapping("hello/{name}")
+    public String helloWithPathParam(@PathVariable String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
+    }
+
+    //lives @ /form
+    @GetMapping("form")
+    public String helloForm() {
+        return "form";
+
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("Valerie");
+        names.add("Reid");
+        names.add("Bentley");
+        names.add("Finley");
+        model.addAttribute("names", names);
+        return "hello-list";
+    }
+
 }
